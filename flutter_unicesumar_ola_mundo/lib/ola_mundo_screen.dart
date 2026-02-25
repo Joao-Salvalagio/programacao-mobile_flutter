3f2bc48ddd2dc7bb69ae9ad2aa3c9b77320da2e0
João Miguel Silva Salvalagio <joaosalvalagio@gmail.com>
2026-02-25 20:21:38 -0300

    25-02-2026

--- flutter_unicesumar_ola_mundo/lib/ola_mundo_screen.dart
+++ flutter_unicesumar_ola_mundo/lib/ola_mundo_screen.dart
@@ -1,15 +1,32 @@
 import 'package:flutter/material.dart';
 
-class OlaMundoScreen extends StatelessWidget {
+class OlaMundoScreen extends StatefulWidget {
   const OlaMundoScreen({super.key});
+
+  @override
+  State<OlaMundoScreen> createState() => _OlaMundoScreenState();
+}
+
+class _OlaMundoScreenState extends State<OlaMundoScreen> {
+  String mensagem = "João Miguel";
+  int contador = 0;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text("Olá, Mundo! Ivo Cuzao"), SizedBox(height: 40),
-        Text("João Miguel o melhor do mundo"),
-      ],
-    );
-  }
-}
+        Text("Olá, Mundo!"), 
+        SizedBox(height: 40),
+        Text(contador.toString()),
+            SizedBox(
+              width: double.infinity,
+              child: FloatingActionButton(onPressed: () {
+                  setState(() {
+                    contador++;
+                  });
+              }, child: Text("Clique Aqui")),
+            )
+          ],
+        );
+      }
+    }
